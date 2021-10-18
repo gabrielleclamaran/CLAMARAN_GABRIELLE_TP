4a860931eb410cfed62e130b90fefa89ffd33c81
gabclamaran <gabclamaran@localhost>
2021-10-18 09:16:19 +0200

    version 1

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,21 @@
+/*
+ GAbrielle 
+ */
+package exo1;
+
+/**
+ *
+ * @author gabclamaran
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        // TODO code application logic here
+    }
+    
+}
